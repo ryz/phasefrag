66ad253405e7e9067751183d2f1585f6aa3f9d05
Steve Haßenpflug <ryzawy@gmail.com>
2015-08-15 03:09:47 +0200

    Lots of changes, Visual Studio integration, etc

---
+++ unity/Assets/Scripts/SimpleEnemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleEnemy : MonoBehaviour {
+
+	public int HP = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(HP <= 0)
+			Death ();
+	}
+
+	public void Hurt()
+	{
+		// Reduce the number of hit points by one.
+		Debug.Log("hurt!");
+		HP--;
+	}
+
+	public void Death() {
+		Destroy(gameObject);
+		Debug.Log("DESTROYED!");
+	}
+
+}
